1a9a51bae7c9faecf260af26ffab6bee1cd9228b
Juan Eduardo Flores <juanedflores@gmail.com>
2020-06-12 13:10:36 -0500

    cleaned up code

--- inline.js
+++ inline.js
@@ -1,4 +1,7 @@
-async function getPage (filename) {
+/*
+ * Get the HTML for the specific menu bar link.
+ */
+function getPage (filename) {
   const fileUrl = filename; // provide file location
 
   fetch(fileUrl)
--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-const rotate = 0;
 let g;
 let myFont;
 const tw = 200;
@@ -11,47 +10,46 @@ let yvalues;
 const xspacing = 21;
 const period = 500.0;
 let dx;
-let canvas;
 
+/*
+ * P5js preload function.
+ */
 function preload () {
   myFont = loadFont("assets/fff-forward.regular.ttf");
 }
 
+/*
+ * P5js setup function.
+ */
 function setup () {
-  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  const canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
+
   g = createGraphics(tw, th);
   g.background(0, 0);
   g.textSize(30);
   g.fill(255);
   g.textFont(myFont);
   g.textAlign(CENTER);
-  const p = 'p';
+
   for (let i = 0; i < th; i += 40) {
     g.text("PHAGES", tw / 2, i);
   }
   makePlanes();
+
   dx = (TWO_PI / period) * xspacing;
 }
 
+/*
+ * P5js draw function.
+ */
 function draw () {
   background(20);
-  // rotateZ(theta);
-  // rotateX(theta);
-  // rotateY(theta);
-  // translate(-800, -900, -1000);
-  const camX = map(mouseX, 0, width, 400, 0);
-  // camera(0, 0, (height / 2) / tan(PI / 6), 0, 0, 0, 0, 1, 0);
-  translate(0, -900, -1000);
-  // camera(0, 0, (height / 2) / tan(PI / 6), 0, 0, 0, 0, 1, 0);
 
-  noStroke();
-  // orbitControl();
+  translate(0, -900, -1000);
 
   amplitude = map(mouseY, 0, width, 400.0, 20.0);
-
-  // stroke(255);
   theta += 0.02;
   let x = theta;
   for (let i = 0; i < yvalues.length; i++) {
@@ -59,7 +57,7 @@ function draw () {
     x += dx;
   }
 
-  lights();
+  noStroke();
   let locY = 0;
   for (let i = 0; i < canvases.length; i++) {
     translate(0, slice + 14, yvalues[i]);
@@ -69,15 +67,22 @@ function draw () {
   }
 }
 
+/*
+ * Create the individual sections of text.
+ */
 function makePlanes () {
   canvases = [];
   for (let y = 0; y < th / slice; y++) {
     canvases[y] = createGraphics(200, slice);
     canvases[y].copy(g, 0, y * slice, 200, slice, 0, 0, 200, slice);
   }
+  // create an array of y values.
   yvalues = new Array(canvases.length);
 }
 
+/*
+ * Resize canvas if window is resized.
+ */
 function windowResized () {
   resizeCanvas(windowWidth, windowHeight);
 }
