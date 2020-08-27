6a61c75fedd8498f02594971d43ba8891d0d891c
juaneduardoflores <juanedflores@gmail.com>
2020-08-27 09:32:17 -0500

    organized scripts in folder

---
+++ scripts/inline.js
@@ -0,0 +1,11 @@
+/*
+ * Get the HTML for the specific navigation bar link.
+ */
+function getPage (filename) {
+  const fileUrl = filename; // provide file location
+
+  fetch(fileUrl)
+    .then(r => r.text())
+    .then(t => document.getElementById("contentArea").innerHTML = t)
+    .then(() => document.getElementById("contentArea").scrollIntoView());
+}
